12d4ab16eda44f0c722392a8f0fbe1c3ec258d6f
Camilla <camillateodoro@hotmail.com>
2023-10-19 16:01:45 -0300

    Add message component and service

--- angular-tour-of-herous/src/app/heroes/heroes.component.ts
+++ angular-tour-of-herous/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +11,16 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent {
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
+  
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
-  this.selectedHero = hero;}
+  this.selectedHero = hero;
+  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+}
 
   getHeroes(): void {
     this.heroService.getHeroes()
